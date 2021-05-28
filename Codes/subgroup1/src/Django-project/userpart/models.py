8d6ddbf2e1f4cca27dcc82bcc28ee9c8603750b4
hanabzu <hanthird@gmail.com>
2021-05-28 17:02:41 +0900

    Create: database models

--- Codes/subgroup1/src/Django-project/userpart/models.py
+++ Codes/subgroup1/src/Django-project/userpart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class AccountInfo(models.Model):
+    accountid = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    position = models.TextField()
+    name = models.TextField()
+    address = models.TextField()
+
+class tempAccountInfo(models.Model):
+    accountid = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    position = models.TextField()
+    name = models.TextField()
+    address = models.TextField()
+
+class log(models.Model):
+    account = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    cond = models.TextField()
