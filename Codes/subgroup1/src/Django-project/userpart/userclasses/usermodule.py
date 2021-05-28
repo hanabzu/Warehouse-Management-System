f8b7ad82395d481ee5b6d3074affbcf3fb91ebb3
hanabzu <hanthird@gmail.com>
2021-05-28 18:10:03 +0900

    Create: UserModule, AccountInfo, CompareAccount 초안

---
+++ Codes/subgroup1/src/Django-project/userpart/userclasses/usermodule.py
@@ -0,0 +1,31 @@
+# user module
+
+def encrypt(accountInfo):
+    eA = accountInfo
+    
+    # encryption of Id, password
+
+    return eA
+
+def decrypt(eA):
+    A = eA
+
+    # decryption of Id, password
+
+    return A
+
+def userData(accountInfo):
+    A = accountInfo
+    if A.flag == True:
+        # dbconnection.deleteOld(A.id)
+    
+    #dbconnection.saveNew(A)
+
+
+def authenticateUser(accountInfo):
+    A = encrypt(accountInfo)
+    
+
+
+
+    return ret
