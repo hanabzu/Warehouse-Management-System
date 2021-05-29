2babe577687c5d03512a9c1e2e25ba655b696c05
hanabzu <hanthird@gmail.com>
2021-05-29 16:58:03 +0900

    Update: compareAccount class

--- Codes/subgroup1/src/Django-project/userpart/userclasses/CompareAccount.py
+++ Codes/subgroup1/src/Django-project/userpart/userclasses/CompareAccount.py
@@ -1,9 +1,30 @@
+from ..models import *
+
 class CompareAccount:
-    def getAccount(self):
-        # get id from db
-        self.dA = from db
+    def __init__(self):
+        self.__dAs = data_AccountInfo.objects.all()
+        self.__ret = "NoMatch"
 
     def compareID(self, A):
+        for dA in self.dAs:
+            if dA.accountid == A._accountid:
+                self.comparePW(self,dA,A)
+
+    def comparePW(self, dA, A):
+        if dA.password == A._password:
+            self.__ret = "Agree"
+        else:
+            self.__ret = "Refuse"
+        self.saveLog(self, dA)
+
+    def saveLog(self, dA):
+        #saveLog to models
+        
+
+
+
+
+
 
 
 
