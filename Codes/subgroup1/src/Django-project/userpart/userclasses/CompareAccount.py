a6b8db988a924c1bf9466f9f77de6fbb50a5f0bd
hanabzu <hanthird@gmail.com>
2021-05-29 21:57:59 +0900

    Update: testing - register

--- Codes/subgroup1/src/Django-project/userpart/userclasses/CompareAccount.py
+++ Codes/subgroup1/src/Django-project/userpart/userclasses/CompareAccount.py
@@ -7,10 +7,12 @@ class CompareAccount:
         self.__ret = "NoMatch"
 
     def compareID(self, A):
-        self.__ret = "123"
         for dA in self.__dAs:
             if dA.accountid == A._accountid:
-                self.comparePW(dA,A)
+                if A._register == True:
+                    self.__ret = "Duplicate"
+                else:
+                    self.comparePW(dA,A)
 
     def comparePW(self, dA, A):
         if dA.password == A._password:
@@ -21,9 +23,8 @@ class CompareAccount:
         if A._register == False:
             self.saveLog(dA)
         
-
     def saveLog(self, accountInfo):
-        #saveLog to models
+        # saveLog to models
         log = data_log(account = accountInfo, time = datetime.now(), cond = self.__ret)
         log.save()
 
