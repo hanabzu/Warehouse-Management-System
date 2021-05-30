c7cdab4af21b89beebb7d17e0f24ac90eca54a4f
boy-min <sonhyunmin00@naver.com>
2021-05-30 11:13:18 +0900

    update controller, transactiondata

--- Codes/subgroup3/14, 19/transaction_data/controller.py
+++ Codes/subgroup3/14, 19/transaction_data/controller.py
@@ -1,7 +1,6 @@
 from transaction_data.database_connection import databaseconnection as dbc
 
 class Controller :
-    def __init__(self, brand, product, user) :
-        self.__transaction_data = {"brand" : brand, "product" : product, "user" : user}
-        self.__db = dbc.DatabaseConnection(self.__transaction_data)
-        del self.__db
+    def __init__(self, transaction_data) :
+        dbc.DatabaseConnection(self.__transaction_data)
+        
--- Codes/subgroup3/14, 19/transaction_data/transactiondata.py
+++ Codes/subgroup3/14, 19/transaction_data/transactiondata.py
@@ -1,4 +1,5 @@
 from transaction_data import controller
 
 def transaction_data(brand, product, user) :
-    controller.Controller(brand, product, user)
+    transaction_data = {"brand" : brand, "product" : product, "user" : user}
+    controller.Controller(transaction_data)
