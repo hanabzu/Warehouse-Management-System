0526bed245084fb7509bc7f4458fd7bc76e0265b
hanabzu <hanthird@gmail.com>
2021-05-31 01:09:09 +0900

    Update: save logout log

--- Codes/subgroup1/src/Django-project/userpart/urls.py
+++ Codes/subgroup1/src/Django-project/userpart/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('success/', views.dosignup, name='dosignup'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/<str:accountid>/', views.logout, name='logout'),
     path('<str:tA_id>/', views.viewTempAccountInfo, name= 'viewTempAccountInfo'),
     path('progressSuccess/', views.progressSuccess, name='progressSuccess'),
 
