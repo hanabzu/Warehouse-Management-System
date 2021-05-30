c7d30135675e70d76795035804d3269b2616e10f
boy-min <sonhyunmin00@naver.com>
2021-05-30 11:41:55 +0900

    update

--- Codes/subgroup3/14, 19/transaction_data/database_connection/archiever/archiever.py
+++ Codes/subgroup3/14, 19/transaction_data/database_connection/archiever/archiever.py
@@ -1,9 +1,16 @@
+import time
+
 def add(database, transaction_data) :
+    t = time.localtime(time.time())
+    
     if transaction_data.get("user")[0] == "warehouse" :
         database.write(transaction_data.get("user")[0] + " " + \
                        str(transaction_data.get("user")[1]) + " " + \
                        "admin" + " " + \
                        str(transaction_data.get("user")[2]) + " " + \
+                       str(t.get('tm_year')) + '/' + \
+                       str(t.get('tm_mon')) + '/' + \
+                       str(t.get('tm_day')) + ' ' + \
                        transaction_data.get("brand") + " " + \
                        transaction_data.get("product") + "\n")
 
@@ -12,5 +19,8 @@ def add(database, transaction_data) :
                        str(transaction_data.get("user")[1]) + " " + \
                        "warehouse" + " " + \
                        str(transaction_data.get("user")[2]) + " " + \
+                       str(t.get('tm_year')) + '/' + \
+                       str(t.get('tm_mon')) + '/' + \
+                       str(t.get('tm_day')) + ' ' + \
                        transaction_data.get("brand") + " " + \
                        transaction_data.get("product") + "\n")
