2bb794494729ba6906c432b740be7f520062b082
boy-min <sonhyunmin00@naver.com>
2021-05-30 11:14:24 +0900

    update controller

--- Codes/subgroup3/14, 19/transaction_data/controller.py
+++ Codes/subgroup3/14, 19/transaction_data/controller.py
@@ -2,5 +2,5 @@ from transaction_data.database_connection import databaseconnection as dbc
 
 class Controller :
     def __init__(self, transaction_data) :
-        dbc.DatabaseConnection(self.__transaction_data)
+        dbc.DatabaseConnection(transaction_data)
         
