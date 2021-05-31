2b82b9819c95a75654d2901495e3e21d49c3168e
Gyong97 <999kgy@naver.com>
2021-05-31 23:21:09 +0900

    UC_09 gui mock up code

--- Codes/subgroup3/UC_09/UC_09_ShopkeeperOrder/controller.py
+++ Codes/subgroup3/UC_09/UC_09_ShopkeeperOrder/controller.py
@@ -26,44 +26,44 @@ class controller:
         self.price = 0
         self.result = None
         ### cli mode 시 ###
-        # if self.user.type == 'shop':
-        #     choice = int(input("1.발주 신청  2. 발주 결과 확인 "))
+        if self.user.type == 'shop':
+            choice = int(input("1.발주 신청  2. 발주 결과 확인 "))
 
-        #     if choice == 1:
-        #         # 발주 신청시
-        #         order_list = self.MakeOrder()
-        #         order_sender = ods.OrderSender(order_list, self.user.connected, self.user.id ,self.user.type)
-        #         order_sender.SendOrder()
-        #     else:
-        #         # 결과확인 시
-        #         # ResultSender모듈을 통해서 결과 확인.
-        #         order_list, result = rs.ResultTaker(user.id).takeResult()
-        #         for i in range(len(order_list)):
-        #             print(order_list[i].order_number, result[i])
-        #         # 결과 확인 후 MOneyChanger로 money 차감.
-        #         mc.MoneyChanger([order_list[i].total_price for i in range(len(order_list))],result,user.id).changeMoney()
+            if choice == 1:
+                # 발주 신청시
+                order_list = self.MakeOrder()
+                order_sender = ods.OrderSender(order_list, self.user.connected, self.user.id ,self.user.type)
+                order_sender.SendOrder()
+            else:
+                # 결과확인 시
+                # ResultSender모듈을 통해서 결과 확인.
+                order_list, result = rs.ResultTaker(user.id).takeResult()
+                for i in range(len(order_list)):
+                    print(order_list[i].order_number, result[i])
+                # 결과 확인 후 MOneyChanger로 money 차감.
+                mc.MoneyChanger([order_list[i].total_price for i in range(len(order_list))],result,user.id).changeMoney()
 
-        # else:
-        #     # 발주 확인 작동 가정
-        #     choice = int(input("1.발주 확인 2. 종료"))
-        #     if choice == 1:
-        #         receive_order_list, sender_list = self.TakeOrder()
-        #         if len(receive_order_list) != 0: # 받은 발주가 있을 때
-        #             order_accepter = oa.OrderAccepter(self.user.id, receive_order_list)
+        else:
+            # 발주 확인 작동 가정
+            choice = int(input("1.발주 확인 2. 종료"))
+            if choice == 1:
+                receive_order_list, sender_list = self.TakeOrder()
+                if len(receive_order_list) != 0: # 받은 발주가 있을 때
+                    order_accepter = oa.OrderAccepter(self.user.id, receive_order_list)
 
-        #             #자동 모드에 따라 발주 확인 및 승인/거절
-        #             if self.user.mode == 'on':
-        #                 order_accepter.AutoChecker()
-        #             else:
-        #                 order_accepter.selfcheck()
-        #             Result = order_accepter.getResult() # Accept/ Reject 결과 리스트 반환 받기.
+                    #자동 모드에 따라 발주 확인 및 승인/거절
+                    if self.user.mode == 'on':
+                        order_accepter.AutoChecker()
+                    else:
+                        order_accepter.selfcheck()
+                    Result = order_accepter.getResult() # Accept/ Reject 결과 리스트 반환 받기.
 
-        #             db.DBconnection(Result, receive_order_list).UpdateStock() # 발주 승인에 따른 재고량 감소.
+                    db.DBconnection(Result, receive_order_list).UpdateStock() # 발주 승인에 따른 재고량 감소.
                     
-        #             # 결과 전송
-        #             rs.ResultSender(sender_list, Result, [receive_order_list[i].order_number for i in range(len(receive_order_list))]).sendResult()
-        #         else:
-        #             print("종료합니다.")
+                    # 결과 전송
+                    rs.ResultSender(sender_list, Result, [receive_order_list[i].order_number for i in range(len(receive_order_list))]).sendResult()
+                else:
+                    print("종료합니다.")
 
 
     def TakeOrder(self):
@@ -80,7 +80,7 @@ class controller:
 #driver
 class initiator:
     def __init__(self,user_type):
-        # user_type = int(input("1. 창고주 / 2. 점주 "))
+        user_type = int(input("1. 창고주 / 2. 점주 "))
         user_info = []
         if user_type == 1:
             user_info.append("warehouse")
@@ -99,6 +99,7 @@ class initiator:
                 user_info.append(temp[2].rstrip('\n'))
             now_user = sh_user(user_info[0],user_info[1],user_info[2],user_info[3])
         self.now_user = now_user
+        controller(self.now_user)
     def return_controller(self):
         return controller(self.now_user)
 
