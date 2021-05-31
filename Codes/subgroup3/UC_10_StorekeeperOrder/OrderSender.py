88674f69dbb7c8e7e616a13fe9f454fcfa800be9
Gyong97 <999kgy@naver.com>
2021-05-31 16:31:24 +0900

    오타 수정

--- Codes/subgroup3/UC_10_StorekeeperOrder/OrderSender.py
+++ Codes/subgroup3/UC_10_StorekeeperOrder/OrderSender.py
@@ -8,8 +8,8 @@ class OrderSender: # 발주를 보내면 주문번호.txt생성
         if mc.MoneyChecker(self.order.total_price).checkBalance(): # 잔고 확인 후 잔고가 충분하면 Send
             order_file = open(self.order.order_number + ".txt", "w", encoding="utf8")
             print("{}".format("Ready"), file = order_file)
-            for i in range(len(self.order_list.item_name)):
-                print("{} {} {} {}".format(self.order_list.item_name[i], self.order_list.item_brand[i], self.order_list.item_price[i], self.order_list.item_number[i]), file = order_file)
+            for i in range(len(self.order.item_name)):
+                print("{} {} {} {}".format(self.order.item_name[i], self.order.item_brand[i], self.order.item_price[i], self.order.item_number[i]), file = order_file)
             order_file.close()
         else:
             print("돈이 부족해서 발주 신청이 취소되었습니다.")
