a69710c2004219d5ab592449a6b1b0bd6944a4b1
Gyong97 <999kgy@naver.com>
2021-05-31 12:40:33 +0900

    make Datecounter

---
+++ Codes/subgroup3/UC_10_StorekeeperOrder/DateCounter.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+class Datecounter:
+    def __init__(self, oreder_date, n_day):
+        self.order_date = oreder_date
+        self.n_day = n_day
+        
+    def DateCheck(self):
+        today = datetime.date.today()
+        today = today.day
+        if self.order_date - self.n_day <= today and self.order_date > today: # n일전 ~ 발주날짜 전날 까지
+            return True
+        else:
+            return False
+
