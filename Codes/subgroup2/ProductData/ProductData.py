2af663ca458e6f29ffafd85736540afe5b9aba3a
박재욱 <80030233+Mesopotamia2957@users.noreply.github.com>
2021-05-31 23:08:41 +0900

    ProductData

--- Codes/subgroup2/ProductData/ProductData.py
+++ Codes/subgroup2/ProductData/ProductData.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
+import sqlite3
 
 
 con = sqlite3.connect("Product.db")
@@ -8,7 +9,8 @@ cur = con.cursor()
 
 
 
-cur.execute("CREATE TABLE IF NOT EXISTS(name TEXT, brand TEXT)")
+cur.execute("CREATE TABLE Product (name TEXT, brand TEXT)")
+cur.execute("INSERT INTO Product VALUES ('새우깡', '농심')")
 
 
 
@@ -24,36 +26,42 @@ class ProductData(Frame):
         self.pack(fill=BOTH, expand=True)
 
         def add():
-            cur.execute("insert into Product values(?,?);",(str(name),str(brand)))
+            name = Entry.get(entryname)
+            brand = Entry.get(entrybrand)
+            cur.execute("INSERT INTO Product values(?,?);",(str(name),str(brand)))
+            con.commit()
 
-        def exit():
-            con.close()
-
-        def edit():
-            con.close()
+        #def edit():
+            #name = Entry.get(entryname)
+            #brand = Entry.get(entrybrand)
+            #cur.execute("INSERT INTO Product values(?,?);",(str(name),str(brand)))
+            #con.commit()
 
         def delete():
-            con.close()
+            name = Entry.get(entryname)
+            brand = Entry.get(entrybrand)
+            cur.execute("DELETE FROM Product WHERE name = ? AND brand = ?", (str(name),str(brand)))
+            con.commit()
 
-        # 성명
+        # 제품명
         frame1 = Frame(self)
         frame1.pack(fill=X)
  
-        name = Label(frame1, text="제품명", width=10)
-        name.pack(side=LEFT, padx=10, pady=10)
+        lblname = Label(frame1, text="제품명", width=10)
+        lblname.pack(side=LEFT, padx=10, pady=10)
  
-        entryName = Entry(frame1)
-        entryName.pack(fill=X, padx=10, expand=True)
+        entryname = Entry(frame1)
+        entryname.pack(fill=X, padx=10, expand=True)    
  
-        # 회사
+        # 브랜드
         frame2 = Frame(self)
         frame2.pack(fill=X)
  
-        brand = Label(frame2, text="브랜드명", width=10)
-        brand.pack(side=LEFT, padx=10, pady=10)
+        lblbrand = Label(frame2, text="브랜드명", width=10)
+        lblbrand.pack(side=LEFT, padx=10, pady=10)
  
-        entryComp = Entry(frame2)
-        entryComp.pack(fill=X, padx=10, expand=True)
+        entrybrand = Entry(frame2)
+        entrybrand.pack(fill=X, padx=10, expand=True)
  
  
         # 추가
@@ -63,10 +71,10 @@ class ProductData(Frame):
         btnAdd.pack(side=LEFT, padx=10, pady=10)
 
         # 수정
-        frame4 = Frame(self)
-        frame4.pack(fill=X)
-        btnEdit = Button(frame4, text="수정", command=edit)
-        btnEdit.pack(side=LEFT, padx=10, pady=10)
+        #frame4 = Frame(self)
+        #frame4.pack(fill=X)
+        #btnEdit = Button(frame4, text="수정", command=edit)
+        #btnEdit.pack(side=LEFT, padx=10, pady=10)
 
         # 삭제
         frame5 = Frame(self)
@@ -74,11 +82,6 @@ class ProductData(Frame):
         btnDelete = Button(frame5, text="삭제", command=delete)
         btnDelete.pack(side=LEFT, padx=10, pady=10)
 
-        # 나가기
-        frame6 = Frame(self)
-        frame6.pack(fill=X)
-        btnDelete = Button(frame6, text="나가기", command=exit)
-        btnDelete.pack(side=LEFT, padx=10, pady=10)
  
  
 def main():
