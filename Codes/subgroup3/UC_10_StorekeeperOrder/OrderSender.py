64113f874ceadd4b94786e665847c87b95f542b7
Gyong97 <999kgy@naver.com>
2021-05-31 16:28:16 +0900

    edit OrderSender(add moneycheck)

--- Codes/subgroup3/UC_10_StorekeeperOrder/OrderSender.py
+++ Codes/subgroup3/UC_10_StorekeeperOrder/OrderSender.py
@@ -1,12 +1,15 @@
 import OrderList
-
+import MoneyChecker as mc
 class OrderSender: # 발주를 보내면 주문번호.txt생성
     def __init__(self, order):
         self.order = order
 
     def SendOrder(self): # 신청 시 Ready 상태로 파일 생성.
-        order_file = open(self.order.order_number + ".txt", "w", encoding="utf8")
-        print("{}".format("Ready"), file = order_file)
-        for i in range(len(self.order_list.item_name)):
-            print("{} {} {} {}".format(self.order_list.item_name[i], self.order_list.item_brand[i], self.order_list.item_price[i], self.order_list.item_number[i]), file = order_file)
-        order_file.close()
+        if mc.MoneyChecker(self.order.total_price).checkBalance(): # 잔고 확인 후 잔고가 충분하면 Send
+            order_file = open(self.order.order_number + ".txt", "w", encoding="utf8")
+            print("{}".format("Ready"), file = order_file)
+            for i in range(len(self.order_list.item_name)):
+                print("{} {} {} {}".format(self.order_list.item_name[i], self.order_list.item_brand[i], self.order_list.item_price[i], self.order_list.item_number[i]), file = order_file)
+            order_file.close()
+        else:
+            print("돈이 부족해서 발주 신청이 취소되었습니다.")
