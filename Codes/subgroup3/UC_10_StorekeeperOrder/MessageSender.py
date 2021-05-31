6d8488e19e111227fa43be77d30445418ce3189a
Gyong97 <999kgy@naver.com>
2021-05-31 16:49:09 +0900

    test_messageSender,

--- Codes/subgroup3/UC_10_StorekeeperOrder/MessageSender.py
+++ Codes/subgroup3/UC_10_StorekeeperOrder/MessageSender.py
@@ -1,25 +1,29 @@
 ## admin이 주문을 보고 승인/거절 결정. stub으로 이용.
 
-
+import os
 class MessageSender:
     def __init__(self, order_num):
         self.order_num = order_num
 
     def AcceptOrder(self):
-        file = open(self.order_num + '.txt', 'r', encoding='utf8')
+        if os.path.isdir(os.getcwd() + '\\' + self.user_id + "_Order") == False:
+            os.mkdir(os.getcwd() + '\\' + self.user_id + "_Order")
+        file = open(os.getcwd() + '\\' + self.user_id + "_Order\\" +self.order_num + '.txt', 'r', encoding='utf8')
         lines = file.readlines()
         lines[0] = 'Accept' # 승인으로 변경
         file.close()
-        file = open(self.order_num + '.txt', 'w', encoding='utf8')
+        file = open(os.getcwd() + '\\' + self.user_id + "_Order\\" +self.order_num + '.txt', 'w', encoding='utf8')
         for line in lines:
             print(line.rstrip('\n'), file= file)
         file.close()
     def RejectOrder(self):
-        file = open(self.order_num + '.txt', 'r', encoding='utf8')
+        if os.path.isdir(os.getcwd() + '\\' + self.user_id + "_Order") == False:
+            os.mkdir(os.getcwd() + '\\' + self.user_id + "_Order")
+        file = open(os.getcwd() + '\\' + self.user_id + "_Order\\" +self.order_num + '.txt', 'r', encoding='utf8')
         lines = file.readlines()
         lines[0] = 'Reject' # 승인으로 변경
         file.close()
-        file = open(self.order_num + '.txt', 'w', encoding='utf8')
+        file = open(os.getcwd() + '\\' + self.user_id + "_Order\\" +self.order_num + '.txt', 'w', encoding='utf8')
         for line in lines:
             print(line.rstrip('\n'), file= file)
         file.close()
