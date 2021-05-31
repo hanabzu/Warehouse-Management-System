dcda1557e880a649ee6603a8caed2ee5c2c99fcb
Gyong97 <999kgy@naver.com>
2021-05-31 13:39:15 +0900

    add driver for load user_info

--- Codes/subgroup3/UC_10_StorekeeperOrder/controller.py
+++ Codes/subgroup3/UC_10_StorekeeperOrder/controller.py
@@ -1,19 +1,22 @@
 import OrderDate as od
 import DateCounter as dc
-
+import OrderMaker as om
+import OrderList
 
 class controller:
-    def __init__(self):
+    def __init__(self, user):
+        self.user = user
         self.orderdate = self.createOrderDate()
         self.isDay = dc.Datecounter(self.orderdate.date, self.orderdate.n_day).DateCheck()
-
-        choice = int(input("1. 발주 날짜 변경 2....."))
+        self.orderList = None
+        choice = int(input("1. 발주 날짜 변경 2.자동 발주 테스트"))
 
         if choice == 1:
             # 발주 날짜 변경 옵션 선택 시
             self.orderdate = self.EditOrderDate()
             self.isDay = dc.Datecounter(self.orderdate.date, self.orderdate.n_day).DateCheck()
-        
+        elif choice == 2:
+            self.orderList = om.OrderMaker()
 
     def createOrderDate(self):
         file = open("창고주.txt", 'r',  encoding='utf8')
@@ -40,9 +43,33 @@ class controller:
         print(lines[3].rstrip('/n'), file= file)
         file.close()
         return od.OrderDate(order_date, order_n_day)
+
+class user:
+    def __init__(self, user_id, user_mode, user_money):
+        self.user_id = user_id
+        self.user_mode = user_mode
+        self.user_money = user_money
+
 class initiator:
     def __init__(self):
-        controller()
+        controller(self.getUser_info())
+
+    def getUser_info(self):
+        file = open("창고주.txt", 'r',  encoding='utf8')
+        lines = file.readlines()
+        file.close()
+        for line in lines:
+            temp = line.rstrip('\n').split(' ')
+            if temp[0] == 'id':
+                user_id = temp[-1] 
+            elif temp[0] == 'auto_mode':
+                user_mode = temp[-1]
+            elif temp[0] == 'money':
+                user_money = temp[-1]
+            else:
+                print("error! 창고주.txt파일 점검 필요!")
+        return user(user_id, user_mode, user_money)
+            
 
 initiator()
 
